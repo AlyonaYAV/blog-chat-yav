ba8bb9b7d7dd7bab91b9701301451f8d9df6c528
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-25 23:12:03 +0300

    Vuetify was installed as a module

---
+++ vuetify.options.js
@@ -0,0 +1,21 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+    theme: {
+        dark: false,
+        themes: {
+
+            light: {
+                primary: '#197602',
+                secondary: colors.teal.lighten3,
+                accent: colors.teal.accent4,
+
+                error: '#ff5252',
+                info: '#2196f3',
+                secces: '#4CAf50',
+                warning: '#ffc107',
+
+            }
+        }
+    }
+};
