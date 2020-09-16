7508082c6affafcb1971786dd2dafc0705a17d4b
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-09-16 22:08:58 +0300

    Temporary usage of class Users as a tiny database

---
+++ api/Users.js
@@ -0,0 +1,33 @@
+class Users {
+  constructor () {
+    this.users = []
+  }
+
+  add (user) {
+    this.users.push(user)
+  }
+
+  get (id) {
+    return this.users.find( (user) => user.id === id )
+  }
+
+  remove (id) {
+    const user = this.get(id)
+    // If 'user' exists remove it
+    if (user) {
+      // Remove user if its 'id' isn't equal to 'id'
+      this.users = this.users.filter( (user) => user.id !== id )
+    }
+    // Returne deleted 'user'
+    return user
+  }
+
+  // Get all users in the room
+  getByRoom (room) {
+    return this.users.filter( (user) => user.room === room )
+  }
+}
+
+module.exports = function () {
+  return new Users()
+}
