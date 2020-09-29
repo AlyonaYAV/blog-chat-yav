d09f2cdecf8de23669bb2ae8c0f4975ded06d814
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-09-29 22:38:52 +0300

    Main layout and page for admin have been created

---
+++ layouts/admin.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-container class="el-wrapper-main-layout">
+        <el-header height="70px" class="el-header-main-layout">
+            Header
+        </el-header>
+        <el-container>
+            <el-aside class="el-aside-main-layout">Aside</el-aside>
+            <el-main>
+                <Nuxt />
+            </el-main>
+        </el-container>
+        <el-footer class="el-footer-main-layout">Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.el-wrapper-main-layout{
+  height: 100vh;
+}
+.el-header-main-layout{
+  padding: 0;
+  background-color:#cc6699;
+}
+.el-aside-main-layout{
+  width: 250px;
+  background-color: #F8E2EC;
+
+}
+.el-footer-main-layout{
+  padding: 0;
+  background-color:#cc6699;
+}
+</style>
