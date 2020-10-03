a8c8e11289e23655cc24f923478f8832a6772ee9
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-10-03 16:43:25 +0300

    When an error occurs, the vuex store is used and admin layout to render it to the user

---
+++ store/error.js
@@ -0,0 +1,17 @@
+export const state = () => ({
+  error: null
+})
+
+export const mutations = {
+  setError (state, error) {
+    state.error = error
+  },
+  clearError (state) {
+    state.error = null
+  }
+}
+
+// Display this error for some users
+export const getters = {
+  error: state => state.error
+}
