8cced486b5630f3ba42bd86ddbfbba1a115234c9
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-11-12 16:45:29 +0200

    Controller and a Route for it are created to check for administrator. The Store action 'userCreation' was created to make a request throug the API

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,46 +1,68 @@
 const bcrypt = require('bcrypt-nodejs')
 const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const User = require('./../models/user')
 const keys = require('./../config/keys')
 
 module.exports.login = async (request, response) => {
   // Search for a User by his login
   const applicant = await User.findOne({ login: request.body.login })
-  if (applicant) {
-    // Compare passwords
-    const passResult = bcrypt.compareSync(Request.body.pass, applicant.password)
-    // When password is correct
-    if (passResult) {
-      // Generate Toten
-      const token = jwt.sign({
-        login: applicant.login,
-        userId: applicant._id
-      }, keys.JWT, { expiresIn: (60 * 60) })
-      // Send token to a User
-      response.status(200).json({ token })
+    if (applicant) {
+      // Compare passwords
+      const passResult = bcrypt.compareSync(request.body.pass, applicant.password)
+      // When password is correct
+      if (passResult) {
+        // Generate Token
+        const token = jwt.sign({
+          login: applicant.login,
+          userId: applicant._id
+        }, keys.JWT, { expiresIn: (60 * 60) })
+        // Send token to a User
+        response.status(200).json({ token })
+      } else {
+        response.status(401).json({ message: 'Password is incorrect' })
+        // response.status(404).json({ message: 'User not found' })
+      }
+
     } else {
-      response.satus(401).json({ message: 'Password is incorrect' })
-      // response.status(404).json({ message: 'User not found' })
+      response.status(404).json({ message: 'User not found' })
     }
-  } else {
-    response.status(404).json({ message: 'User not found' })
-  }
 }
 module.exports.createUser = async (request, response) => {
-  const applicant = await User.findOne ({ Login: request.body.login })
-  // If User with this login already exist
+  const applicant = await User.findOne({ login: request.body.login })
+  // If User with this login already exists
   if (applicant) {
     response.status(409).json({ message: "User login is busy" })
   } else {
-    const salt = bcrypt().genSaltSync(10)
+    const salt = bcrypt.genSaltSync(10)
     // Create new User
-    const user = new User({ 
+    const user = new User({
       login: request.body.login,
-      password: bcrypt.hashSync(request.body.pass, salt)
-     })
-     // Save user to DB
-     await user.save()
-     // Something successfully created
-     response.status(201).json(user)
+      password: bcrypt.hashSync(request.body.pass, salt),
+      role: request.body.role
+    })
+    // Save user to Db
+    await user.save()
+    // Something successfully created
+    response.status(201).json(user)
   }
+}
+// If collection 'users' isn't empty there must be an 'admin'
+module.exports.isAdmin = async (request, response) => {
+  await User.countDocuments({}, async (err, amount) => {
+    // If document (collection) isn't empty
+    if (amount) {
+      const admin = await User.findOne({ isAdmin: true })
+      // Admin exists
+      if (admin) {
+        // If admin presents in collection
+        response.status(200).json({'isAdmin': true})
+      } else { 
+        // It's weird the collection isn't empty but admin is absent
+        response.status(200).json({'isAdmin': false})
+      }
+    } else {
+      // Document (collection) is empty
+      response.status(200).json({'isAdmin': false})
+    }
+  })
 }
--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,6 @@
 export const state = () => ({
-  token: null
+  token: null,
+  testDataUser: null
 })
 
 export const mutations = {
@@ -8,6 +9,9 @@ export const mutations = {
   },
   clearToken (state) {
     state.token = null
+  },
+  testSetDataUser (state, dataUser) {
+    state.testDataUser = dataUser
   }
 }
 
@@ -15,9 +19,11 @@ export const actions = {
   // Authorize in the system
   login ({ commit, dispatch }, data) {
     try {
-      const { token } = this.$axios.$post('/api/auth/admin/login', data)
-      console.log('Token ', token)
-        /*eslint-disable*/
+      // const { token } = this.$axios.$post('/api/auth/admin/login', data)
+      const token = 'token-test'
+      commit('testSetDataUser', data)
+      // console.log('Token ', token)
+      /* eslint-disable */
       dispatch('setToken', token)
     } catch (e) {
       // Commit mutation
@@ -34,9 +40,12 @@ export const actions = {
   async userCreation ({ commit }, data) {
     // Request
     try {
-      console.log('User created ', data)
+      await this.$axios.$post('/api/auth/admin/create', data)
+      // console.log('User created ', data)
     } catch (e) {
-
+      // Commit mutation, 'e' - axios is an object error
+      commit('error/setError', e, {root: true})
+      throw e
     }
   }
 }
