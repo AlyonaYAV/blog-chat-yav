2b0e5762955105aa49d36b851240327fb387c654
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-01-22 18:13:59 +0200

    Route for a User forgot-password is ready

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,17 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require('express');
+const router = Router();
 const validationRegistration = require('./../middleware/regisration-user');
 const { register } = require('./../controllers/register');
 const { emailVerification } = require('./../controllers/email-verification');
+const validationForgotPassword = require('./../middleware/forgot-password-user');
+const { forgotPassword } = require('./../controllers/forgotPassword');
 
 // first time registration '/api/user/registration'
 router.post('/registration', validationRegistration(), register);
 // Emeil verification
 //'/api/user/verification'
 router.post('/verification', (req,res,next)=>next(), emailVerification);
+//'/api/user/forgot-password'
+router.post('/forgot-password', validationForgotPassword(), forgotPassword);
 
 module.exports = router
