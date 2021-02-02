b266f77ad31f7cd735d8cf771f43852726aa7f32
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-02-03 00:39:44 +0200

    Helper 'Refresh token' removes own duplicates in DB

--- api/helpers/jwt-refresh.js
+++ api/helpers/jwt-refresh.js
@@ -2,6 +2,11 @@ const { JwtRefresh } = require('./../helpers/db');
 const { randomTokenString } = require('./../helpers/crypto');
 
 function generateRefreshToken(user, ipAddress) {
+    JwtRefresh.deleteOne({ user: user.id }, function(err){
+      if(err){
+          throw new Error("Token refresh exception");
+      }
+    });
     // Create a refresh token that expires in 7 days
     return new JwtRefresh({
         user: user.id,
