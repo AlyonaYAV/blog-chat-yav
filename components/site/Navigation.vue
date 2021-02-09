88756821fee75448a97cd2a0ad09dcac80e0781d
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-02-09 02:06:59 +0200

    The navigation component is modified to conditionally display one of the two child components such as Auth or User panel

--- components/site/Navigation.vue
+++ components/site/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>
+    <nav class="el-menu-pets-nav">
         <el-menu
             router
             :default-active="$route.path"
@@ -28,51 +28,96 @@
             </el-submenu>
             <el-menu-item index="/about" class="el-menu-pets__item">About</el-menu-item>
             <el-menu-item index="/contacts" class="el-menu-pets__item">Contacts</el-menu-item>
-            <el-menu-item index="/chat" class="el-menu-pets__item">Chat</el-menu-item>
         </el-menu>
+        <div class="el-menu-pets__user-space">
+          <Authentication v-if="!isAuth" />
+          <CabAndChat v-else :userLogin="userLogin" />
+        </div>
     </nav>
 </template>
 
 <script>
 /* eslint-disable */
+import Authentication from '@/components/site/user_space/Authentication';
+import CabAndChat from '@/components/site/user_space/CabAndChat';
+import { mapState } from 'vuex';
   export default {
+    components: {
+      Authentication,
+      CabAndChat
+    },
     data() {
       return {
-        activeIndex1: '2'
+        activeIndex1: '2',
+        isAuth: false
       };
+    },
+    computed: {
+      authUser(){
+        return this.$store.getters['auth/isUserAuthenticated'];
+      },
+      ...mapState('auth',{
+        userLogin: (state)=>{ return state.user.login; }
+      })
+    },
+    watch:{
+      authUser(val){
+        this.isAuth = val.login === '' ? false : true;
+      }
     },
     methods: {
       handleSelect(key, keyPath) {
-        console.log(key, keyPath);
+        //console.log(this.$router.name);
       }
+    },
+    created(){
+      if(this.$store.state.auth.user){
+        if(this.$store.state.auth.user.login === ''){
+          this.isAuth = false;
+        }else{
+          this.isAuth = true;
+        }
+      }else{
+        this.isAuth = false;
+      };
     }
   }
 </script>
 
 <style lang="scss">
+  .el-menu-pets-nav::after{
+    content: '';
+    display: block;
+    clear: both;
+  }
+  $menu-height: 70px;
   .el-menu-pets{
     background: #cc6699 url("/paws.png") 0 0/140px 70px no-repeat padding-box border-box scroll !important;
-    height:70px;
+    height: $menu-height;
+    width: 80%;
+    float: left;
     display:flex;
     flex-direction: row;
     justify-content: space-between;
+    .el-menu-pets__item:first-child{
+      margin-left: 1%
+    }
   }
   .el-menu-pets__item{
     height:100% !important;
     font-size: 1rem;
     font-weight: bold;
   }
-  /*.el-menu-pets__item{
-    height:100% !important;
-    font-size: 1rem;
-    margin-left: 10% !important;
-  }
-   .el-menu-pets__item_sub{
-     margin-left:10%;
-   }*/
   .el-menu-pets__item_sub div.el-submenu__title{
     height:68px !important;
     font-size: 1rem;
     font-weight: bold;
+  }
+  .el-menu-pets__user-space{
+    background-color:  #f886c5;
+    background-image: radial-gradient(#f7e5ef, #cc6699);
+    width: 20%;
+    height: $menu-height;
+    float: right;
   }
 </style>
