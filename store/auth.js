c221ed622924cbd090d36b805436a36b31d12c47
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-02-23 20:11:48 +0200

    Action in store has been created 'auth' to refresh token and get an access token

--- store/auth.js
+++ store/auth.js
@@ -17,6 +17,9 @@ export const mutations = {
       role: '',
       jwtToken: ''
     }
+  },
+  setActiveToken(state, activeToken){
+    state.user.jwtToken = activeToken;
   }
 }
 
@@ -24,28 +27,42 @@ export const actions = {
   // Authorize in the system
   async login ({ commit, dispatch }, data) {
     try {
-      const result = await this.$axios.$post('/api/auth/user/login', data)
+      const result = await this.$axios.$post('/api/auth/user/login', data);
       const { jwtToken, login, role, blogBan } = result.details;
       //
       if(blogBan){
         throw new Error('Enter is forbidden');
       }
-      commit('authUser',{ jwtToken, login, role, blogBan });
+      //Only client side rendering
+      //this.$axios.setToken(jwtToken, 'Bearer');
+      commit('authUser', { jwtToken, login, role, blogBan });
     } catch (e) {
       // Commit mutation
-      // console.log('Error ', e.response.data.message);
+      //console.log('Error ', e.response.data.message);
       commit('error/setError', e, {root: true})
       throw e
     }
   },
   logout ({ commit }) {
+    //Only client side rendering
+    //this.$axios.setToken(false);
     commit('userLogOut');
+  },
+  async refreshToken({ commit }){
+    //An Active token has gotten and Refresh token has regenerated
+    const res = await this.$axios.$post('/api/auth/user/refresh');
+    if(res){
+      commit('setActiveToken', res.details.jwtToken);
+    }else{
+      // Refresh is expired
+      commit('error/setError','You should sign in',{root: true});
+    }
   },
   async userCreation ({ commit }, data) {
     // Request
     try {
       await this.$axios.$post('/api/auth/admin/create', data)
-      // console.log('User created ', data)
+      // console.log('User created', data)
     } catch (e) {
       // Commit mutation, 'e' - axios is an object error
       commit('error/setError', e, {root: true})
