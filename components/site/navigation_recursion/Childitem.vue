cd95c2d092508181d263c65afc63d1d12d9e3d2b
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-03-17 01:47:02 +0200

    Two components that create recursive menu for the 'pets_life' directory and based on the 'Element UI' are completed

---
+++ components/site/navigation_recursion/Childitem.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-submenu v-if="child && child.childrenItems.length" :index="'/pets_life/'+child.id">
+    <template slot="title">{{ child.pageName }}</template>
+    <el-menu-item :index="'/pets_life/'+child.id">
+      {{ child.pageName +'( Full list)' }}
+    </el-menu-item>
+    <node
+      v-for="curChild in child.childrenItems"
+      :key="curChild.id"
+      :child="curChild"
+      :index="'/pets_life/'+curChild.id">
+      {{ curChild.pageName+' Test' }}
+    </node>
+  </el-submenu>
+  <el-menu-item v-else :index="'/pets_life/'+child.id">
+    {{ child.pageName }}
+  </el-menu-item>
+</template>
+ 
+ <<script>
+ export default {
+    name: "node",
+    props:{ child: Object },
+ }
+ </script>
+
+ <<style lang="scss" scoped>
+ 
+ </style>
