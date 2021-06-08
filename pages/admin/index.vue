6442175eaebe83464032419b8ebe91cbb71cb321
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-06-09 00:11:05 +0300

    Admin page 'index.vue' has been protected by the function from plugin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -11,21 +11,37 @@ import { mapState } from 'vuex'
 export default {
   async asyncData(context){
     let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
+    // '$isAllowedByRole' is a function from plugin
     let userData = await context.app.$isAllowedByRole(jwt);
-    
     if(userData.role === 'guest'){
       context.redirect('/cabinet');
     }
-    if(userData.role === ''){
+    if(userData.role === '' && !userData.sessionEnd){
       if(context.store.getters['auth/isUserAuthenticated'].role !== ''){
         context.store.dispatch('auth/logout');
       }
       context.redirect('/');
     }
-    return { jwt };
+    return {
+      userLogoutRefresh: userData.sessionEnd ? true : false
+    };
   },
   layout: 'admin',
-  name: 'admin'
-  //computed: mapState('auth', ['testDataUser'])
+  name: 'admin',
+  //computed: mapState('auth', ['testDataUser']),
+  mounted(){
+    // Displaying the modal window to inform user about the end of the session
+    if(this.userLogoutRefresh){
+      this.$alert('Your session is up!', 'Sesion state', {
+        confirmButtonText: 'Sign in again',
+        showClose: false,
+        callback: action => {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login?message=unauthenticated');
+        }
+      });
+    }
+  }
+
 }
 </script>
