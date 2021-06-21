fd26b25c67e974c6d5a7e702c74f575d39ffc27f
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-06-21 22:46:02 +0300

    Nuxt chat layout to display errors from pages has been changed

--- layouts/chat/index.vue
+++ layouts/chat/index.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-      <v-container fluid class="container-chat">
+        <v-container fluid class="container-chat">
           <header-chat></header-chat>
           <v-row>
             <v-col xs="12" md="12" lg="12">
@@ -9,9 +9,9 @@
               </v-container>
             </v-col>
           </v-row>
-      </v-container>
-        <v-footer app :padless="true" color="#cc6699" :height="50">
-            <p>Footer</p>
+        </v-container>
+        <v-footer class="el-footer-main-layout" app :padless="true" color="#225AA4" :height="60">
+          <p>Footer</p>
         </v-footer>
     </v-app>
 </template>
@@ -21,14 +21,40 @@ import HeaderChat from './../../components/site/chat/Header-Chat';
 export default {
   components: {
     HeaderChat
+  },
+  computed: {
+    error () {
+      return this.$store.getters['error/error']
+    }
+  },
+  watch: {
+    error (val) {
+      // $axios - val.response.data.message, val - native JS Error
+      val = val.response ? val.response.data.message : val;
+      if(val.jwtRefreshed){
+        const h = this.$createElement;
+        this.$message({
+          message: h('p', null, [
+            h('span', null, val.jwtRefreshed),
+            h('i', { style: 'color: teal' }, ' Continue to use the interface')
+          ]),
+          duration: 7000
+        });
+      }else{
+        this.$message({
+          showClose: true,
+          message: val,
+          type: 'error'
+        })
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
 .el-footer-main-layout{
   padding: 0;
-  background-color:#cc6699;
+  background-color:#cc6699!important;
 }
 </style>
