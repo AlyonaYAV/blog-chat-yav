187f34006ea266803b5ac0957626a8805727e16b
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-07 23:03:40 +0300

    Object property 'id' that stores in class Users has been changed to 'socketId'. 'createMessage' event saves data into the DB

--- api/api.socket.js
+++ api/api.socket.js
@@ -6,6 +6,7 @@ const httpServer = require('http').createServer(app)
 const io = require('socket.io')(httpServer)
 //
 const users = require('./Users')()
+const { ChatMessage } = require('./helpers/db');
 
 io.on('connection', socket => {
   /* When the user first came (create new User) */
@@ -21,14 +22,14 @@ io.on('connection', socket => {
       // Add a user to the class Users but remove before
       users.remove(socket.id)
       users.add({
-        id: socket.id,
+        socketId: socket.id,
         name: dataUser.name,
         room: dataUser.room
       })
       // If validation is good
       callback({
         // Socket ID is to define the User on the front-end
-        userId: socket.id
+        userSocketId: socket.id
       })
       // Update list of all users in the room
       io.to(dataUser.room).emit('updateUsers', users.getByRoom(dataUser.room))
@@ -50,15 +51,27 @@ io.on('connection', socket => {
         return callback('A message can\'t be empty')
       }
       //
-      const user = users.get(data.id)
+      const user = users.get(data.userSocketId)
       if (user) {
         // to( 'room number' ) - wich room a message is being sent to
         // Event for all users in a specific room
         io.to(user.room).emit('newMessage', {
-          id: data.id,
+          userSocketId: data.userSocketId,
           name: user.name,
           text: data.text
         })
+        // Save current message into the DB
+        ChatMessage.create({
+          text: data.text,
+          user: data.userId,
+          room: data.roomId
+        }, function (err, success){
+          if(err){
+            //console.log("Error ",err);
+          }else{
+            //console.log("Success ",success);
+          }
+        });
         // Call the callback() to clean the form text field
         callback()
       }
