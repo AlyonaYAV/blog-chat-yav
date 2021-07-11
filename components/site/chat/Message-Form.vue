872e1103383d1075cb3c6a5107ca1939cd27e0cd
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-11 20:48:26 +0300

    The Component Message-Form sends the data that has been updated

--- components/site/chat/Message-Form.vue
+++ components/site/chat/Message-Form.vue
@@ -10,9 +10,11 @@
 
 <script>
 export default {
-  props: {
+  props:{
     userId: { type: String, default: '' },
-    roomId: { type: Object }
+    roomId: { type: Object },
+    avatar: { type: String, default: '' },
+    role: { type: String, default: '' }
   },
   data () {
     return {
@@ -21,12 +23,12 @@ export default {
   },
   methods: {
     addMessage () {
-      // console.log('Result is: ', this.formText)
       this.$socket.emit('createMessage', {
-        userSocketId: this.$store.state.chat.user.userSocketId,
         userId: this.userId,
         roomId: this.roomId.id,
-        text: this.formText
+        text: this.formText,
+        avatar: this.avatar,
+        role: this.role
       }, (data) => {
         if (typeof data === 'string') {
           console.error(data)
@@ -37,14 +39,14 @@ export default {
     }
   },
   created () {
-    // this listener is not needed in SSR-mode - if (process.client) } {}
+    // This listener is not needed in SSR-mode - if (process.client) {}
     this.$eventBus.$on('addMessage', () => {
       // Add a message by button's click
       this.addMessage()
     })
   },
   beforeDestroy () {
-    // Make sure to always unsubscribee from events when longer needad
+    // make sure to always unsubscribe from events when no longer needed
     this.$eventBus.$off('addMessage')
   }
 }
