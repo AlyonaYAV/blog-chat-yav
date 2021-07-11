86eb5c161a8959afa1e901bc108b6094018c7404
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-11 20:05:59 +0300

    Two properties are added to 'chat-message' Model

--- api/models/chat-message.js
+++ api/models/chat-message.js
@@ -4,12 +4,15 @@ const { Schema, model } = require('mongoose')
 const chatMessageSchema = new Schema({
   userName: { type: String, required: true },
   text: { type: String, required: true },
+  avatar: { type: String, default: '' },
+  role: { type: String, default: '' },
   date: { type: Date, default: Date.now },
   divider: { type: Boolean, default: false },
   inset: { type: Boolean, default: false },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   room: { type: Schema.Types.ObjectId, ref: 'Chat-room' }
-  /* // Relationship with 'chat-rooms' - many-to-many
+  /*
+  // Relationship with 'chat-rooms' - many-to-many
   rooms: [
     { type: Schema.Types.ObjectId, ref: 'Chat-room' }
   ]*/
