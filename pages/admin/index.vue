6bf791aa4ac1b59ac3c22506161a6a3445e56608
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-21 19:33:15 +0300

    The main page of admin panel displays section to manipulate with the site logo

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
-    <h1>Welcome to admin page</h1>
-    <NuxtLink to="/">Home</NuxtLink>
-  </div>
-
+    <div>
+      <NuxtLink to="/">Home</NuxtLink>
+      <el-tabs type="card" @tab-click="handleTabs">
+        <el-tab-pane label="Admin info">
+          <h1>Welcome to the admin page</h1>
+        </el-tab-pane>
+        <el-tab-pane v-if="user.role === 'admin'" label="Site logo">
+          <logo :imageLink="logo" :key="reloadLogo" />
+        </el-tab-pane>
+      </el-tabs>
+    </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Logo from './../../components/admin/site_logo/Logo.vue';
 export default {
   async asyncData(context){
     let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
-    // '$isAllowedByRole' is a function from plugin
+    //'$isAllowedByRole' is a function from Plugin
     let userData = await context.app.$isAllowedByRole(jwt);
     if(userData.role === 'guest'){
       context.redirect('/cabinet');
@@ -22,19 +29,44 @@ export default {
       }
       context.redirect('/');
     }
-    return {
-      userLogoutRefresh: userData.sessionEnd ? true : false
+    let siteOptions;
+    if(userData.role === 'admin'){
+      siteOptions = await context.app.$getAllSiteOptions(jwt);
+      if(siteOptions){
+        return {
+          userLogoutRefresh: userData.sessionEnd ? true : false,
+          logo: siteOptions?.options?.siteLogo
+        };
+      }
+    }else{
+      return {
+      userLogoutRefresh: userData.sessionEnd ? true : false,
+      logo: siteOptions
     };
+    }
   },
   layout: 'admin',
   name: 'admin',
-  //computed: mapState('auth', ['testDataUser']),
+  components: {
+    Logo
+  },
+  computed: mapState('auth', ['user']),
+  data(){
+    return {
+      activeName: 'first' ,
+      logo:'',
+      reloadLogo: 0
+    }
+  },
+  methods:{
+    async handleTabs(tab, event) {}
+  },
   mounted(){
-    // Displaying the modal window to inform user about the end of the session
+    //Displaying the modal window to inform user about the end of the session
     if(this.userLogoutRefresh){
-      this.$alert('Your session is up!', 'Sesion state', {
+      this.$alert('Your session is up!', 'Session state', {
         confirmButtonText: 'Sign in again',
-        showClose: false,
+        showClose:false,
         callback: action => {
           this.$store.dispatch('auth/logout');
           this.$router.push('/login?message=unauthenticated');
@@ -42,6 +74,5 @@ export default {
       });
     }
   }
-
 }
 </script>
