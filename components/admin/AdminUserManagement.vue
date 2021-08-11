a1828a91327acfe68a561dd3e3ffd584c262fc18
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-08-11 18:39:10 +0300

    The component 'AdminUserManagement' compares data from DB and got by Socket. The result is written into the 'users' property

--- components/admin/AdminUserManagement.vue
+++ components/admin/AdminUserManagement.vue
@@ -2,15 +2,18 @@
   <el-tabs type="card" @tab-click="handleTabPaneClick">
     <el-tab-pane v-bind:label="tabLabels[0]">
       <h3>About user management</h3>
-      <p>In this section you can create new user and modify those who are existed</p>
+      <p>In this section you can create new user and modify those who are existed.</p>
     </el-tab-pane>
     <el-tab-pane v-bind:label="tabLabels[1]">
       <h3>List of users</h3>
       <el-button v-if="!Array.isArray(users) || (users === null)" @click="showUsersList">
         Retry to download user list
       </el-button>
-      <admin-user-list :users="users" :userId="userId" />
-      </el-tab-pane>
+      <admin-user-list
+        :users="users"
+        :userId="userId"
+        v-if="Array.isArray(users) || (users !== null)" />
+    </el-tab-pane>
     <el-tab-pane v-bind:label="tabLabels[2]" v-if="isUserAuthenticated.role === 'admin'">
       <h3>Create user</h3>
       <admin-user-create />
@@ -32,7 +35,7 @@ export default {
         "List of users",
         "Create user"
       ],
-      users: null,
+      users: [],
       userId: ''
     }
   },
@@ -40,23 +43,25 @@ export default {
     ...mapGetters('auth',['isUserAuthenticated'])
   },
   methods: {
-    async getAllUsers(){
+    async getAllUsers(callback){
       try{
         const apiUsers = await this.$axios.get('/api/auth/admin/users');
-        if(apiUsers){ //apiUsers - [ [],'' ]
-          //create array of data for the Moderstors only
+        if(apiUsers){//apiUsers - [ [],'' ]
+          //Create array of data for the Moderators only
           const arrForModerator = [];
-          // Prepare data  for the Moderators
+          //Prepare data for the Moderators
           apiUsers.data.users[0].forEach(obj => {
-            if((obj.role === "guest")) {
+            if((obj.role === "guest")){
               arrForModerator.push(obj);
             }
           });
-          // Get the current role from Vuex Store
+          //Get the current role from Vuex Store
           if(this.isUserAuthenticated.role === "admin"){
             this.users = apiUsers.data.users[0];
+            if(callback) callback(this.users);
           }else{
             this.users = arrForModerator;
+            if(callback) callback(this.users);
           }
           this.userId = apiUsers.data.users[1];
         }
@@ -65,19 +70,62 @@ export default {
       }
     },
     handleTabPaneClick (tab, event) {
-      //console.log(tab, event)
       if(tab.label === this.tabLabels[1]){
-        this.getAllUsers();
+        this.getAllUsers(function(usersFromDb){
+          //Socke connection
+          this.$socket.emit('fromAdminPanel', {}, (data) => {
+              // Server response if request was bad
+              if (typeof data === 'string') {
+                console.error(data);
+              } else {
+                // Good response
+                this.displayChatUserState(data.allUsersInRooms, usersFromDb);
+              }
+            });
+        }.bind(this));
       }
     },
+    displayChatUserState(usersInChat, usersFromDb){
+      if(usersInChat.length > 0){
+        // Reset 'inChat' and 'chatRoom' by default
+        for(let i = 0;i < usersFromDb.length; i++){
+          usersFromDb[i] = {...usersFromDb[i], inChat: false, chatRoom: ''};
+        }
+        for(let i = 0;i < usersFromDb.length; i++){
+          for(let j = 0;j < usersInChat.length; j++){
+            let obj = null;
+            //Find a user from chat
+            if(usersFromDb[i].id === usersInChat[j].userId){
+              obj = { ...usersFromDb[i], inChat: true, chatRoom: usersInChat[j].room };
+            }else{
+              obj = { ...usersFromDb[i] };
+            }
+            usersFromDb[i] = obj;
+          }
+        }
+      }else{
+        //If no one user is in the chat
+        for(let i = 0;i < usersFromDb.length; i++){
+          usersFromDb[i] = { ...usersFromDb[i], inChat: false, chatRoom: '' };
+        }
+      }
+      //Rewrite users property after Socket response
+      this.users = usersFromDb;
+    },
     showUsersList(){
-      this.getAllUsers();
+      this.getAllUsers(function(usersFromDb){
+          //Socke connection
+          this.$socket.emit('fromAdminPanel', {}, (data) => {
+              // Server response if request was bad
+              if (typeof data === 'string') {
+                console.error(data);
+              } else {
+                // Good response
+                this.displayChatUserState(data.allUsersInRooms, usersFromDb);
+              }
+            });
+        }.bind(this));
     }
-  },
-  mounted(){
-    setTimeout(()=>{
-      this.getAllUsers();
-    },3000);
   }
   /* mounted () { // This method is only called on the client side
     // Get query parameter 'message'
@@ -94,20 +142,5 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.el-tabs__item.is-top{
-  &.is-active{
-    color: #cc6699;
-  }
-  &:hover{
-    color: #cc6699;
-  }
-}
-.el-button.el-button--default{
-  background-color: #df7cc1;
-  color: #000;
-  &:hover{
-    color:#fff;
-  }
-}
+<style lang="scss" scoped>
 </style>
