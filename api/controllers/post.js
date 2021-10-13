d1590da1ce47e8ef17e36e90876d8908fde21032
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-10-13 03:11:16 +0300

    Model 'post' and actions 'createPost', 'getPosts' in 'post' controller have been changed. The property 'user' added to the 'post' model and this property is filled with an user object when 'post' creating. Duplication of 'getPosts' deleted

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -4,11 +4,13 @@ const { Post, Comment } = require('./../helpers/db');
 
 // Create new Post - in Admin
 module.exports.createPost = async (request, response) => {
+  const { id:userId } = request.user;
   const post = new Post({
     title: request.body.title,
     text: request.body.text,
     // Get the file path of an uploaded image by Multer
-    imageUrl: `${request.file.filename}`
+    imageUrl: `${request.file.filename}`,
+    user: userId
   })
   try {
     await post.save();
@@ -21,12 +23,37 @@ module.exports.createPost = async (request, response) => {
 
 // Get all Posts in Admin and Public
 module.exports.getPosts = async (request, response) => {
+  const role = request.user ? request.user.role : undefined;
   try {
-    const posts = await Post.find({});
-    if(!posts.length){
-      return response.status(200).json({ message: "No posts", posts: [] });
+    let posts;
+    //On admin side
+    if((role && role === 'admin') || (role && role === 'moderator')){
+      posts = await Post.find({}).populate('user');
+      if(!posts.length){
+        return response.status(400).json({ message: "No posts", posts: [] });
+      }
+      posts = posts.map( post =>{
+        let newPost = {
+          _id: post._id,
+          views: post.views,
+          comments: post.comments,
+          title: post.title,
+          date: post.date,
+          user: {
+            name: post.user.login,
+            role: post.user.role
+          }
+        }
+        return newPost;
+      });
+    }else{
+    //On client side
+      posts = await Post.find({});
+      if(!posts.length){
+        return response.status(200).json({ message: "No posts", posts: [] });
+      }
     }
-    // Sort posts in reverse order 
+    // Sort posts in reverse order
     posts.sort(function(a,b){
       // Turn your strings into dates, and then subtract them
       // to get a value that is either negative, positive, or zero.
@@ -34,49 +61,49 @@ module.exports.getPosts = async (request, response) => {
     });
     response.status(200).json({ message: "All posts", posts });
   } catch (e) {
-    response.status(400).json({ message: "Error", posts: []});
+    response.status(400).json({ message: "Error" , posts: [] });
   }
 }
 
 // Get Post by ID in Admin
 module.exports.getAdminPostById = async (request, response) => {
   const id = request.params.id;
-  if(!id) return response.status(400).json({ message: "Invalid post data", post: {} });
+  if(!id) return response.status(400).json({ message: "Invalid post data" , post: {} });
   try {
     const post = await Post.findById(id).exec();
     if(post){
-      return response.status(200).json({ message: "The post data", post });
+      return response.status(200).json({ message: "The post data" , post });
     }
-    throw new Error("Somthing went wrong");
+    throw new Error("Something went wrong");
   } catch (e) {
     return response.status(400).json({ message: e.message, post: {} });
   }
 }
 
-module.exports.updatePost = async (req, res) =>{
+module.exports.updatePost = async (req, res)=>{
   if(req.body.imagePostToDelete){
-    // Remove previous image by name 'imlagePostDelete'
+    //Remove previous image by name 'imagePostToDelete'
     removeFile(req.body.imagePostToDelete);
   }
-  // Parse the JSON data from 'allData' field - it is an Object of all client data except image
+  //Parse the JSON data from 'allData' field - it is an Object of all client data except image
   let body = JSON.parse(req.body.postData);
   const postUpdateData = {};
   let { title, text } = body;
   postUpdateData.title = title;
   postUpdateData.text = text;
-  // Get 'id' of 'Post' Model
+  //Get 'id' of 'Post' Model
   const id = req.params.id;
-  // 'singleImage' is undefined because it comes through the  Multer
+  //'singleImage' is undefined because it comes through the Multer
   if(!req.body.singleImage){
-    // If an image is sent, its file name is checked
+    //If an image is sent, its file name is checked
     if(req.file){
       postUpdateData.imageUrl = req.file.filename;
     }
-  }else{// 'imagePostToDelete' has a name
+  }else{//'imagePostToDelete' has a name
     postUpdateData.imageUrl = req.body.imagePostToDelete;
   }
   try{
-    // Update Models
+    //Update Models
     const postUpdated = await Post.findOneAndUpdate(
       { _id: id },
       { ...postUpdateData },
@@ -91,64 +118,46 @@ module.exports.updatePost = async (req, res) =>{
 // Delete Post by ID in Admin
 module.exports.deletePost = async (request, response) => {
   const id = request.params.id;
-  if(!id) return response.status(400).json({ message: 'Wrong request' });
+  if(!id) return response.status(400).json({ message: "Wrong request" });
   try {
     const postWithComments = await Post.findById(id).populate('comments').exec();
     if(!postWithComments || !postWithComments.imageUrl){
-      return response.status(400).json({ message: 'Wrong request' });
+      return response.status(400).json({ message: "Wrong request" });
     }
-    // Delete the Post image
+    //Delete the Post image
     removeFile(postWithComments.imageUrl);
     const commentIds = [];
     if(postWithComments.comments && !!postWithComments.comments.length){
-      //Get the all Comments ids
+      //Get the all Comment ids
       postWithComments.comments.forEach(el => {
         commentIds.push(el._id);
       });
     }
-    // Removing data from Comment Model
+    //Removing data from Comment Model
     if(!!commentIds.length){
       await Comment.deleteMany({ _id: { $in : commentIds } });
     }
-    // Delete the Post itself
-    const postDeleted = await Post.deleteOne({ _id: id });
+    //Delete the Post itself
+    const postDeleted = await Post.deleteOne({ _id: id});
     if(postDeleted){
-      return response.status(200).json({ message: 'Post deleted', postId: id });
+      return response.status(200).json({ message: "Post deleted", postId: id });
     }
-  }catch (e) {
+  } catch (e) {
     return response.status(400).json({ message: e.message });
   }
 }
-// Get all client side Posts 
-module.exports.getPosts = async (request, response) => {
-  try{
-    const posts = await Post.find({});
-    if(!posts.length){
-      return response.status(400).json({ message: "No posts", posts: [] });
-    }
-    // Sort posts in reverse order 
-    posts.sort(function(a,b){
-      // Turn your strings into dates, and then subtract them
-      // to get a value that is either negative, positive, or zero.
-      return new Date(b.date) - new Date(a.date);
-    });
-    return response.status(200).json({ message: "All posts", posts });
-  }catch(e){
-    return response.status(400).json({ message: e.message, posts: [] });
-  }
-}
 
-// Get Post by ID on client
+// Get Post by ID on client side
 module.exports.getClientPostById = (request, response) => {
   const id = request.params.id;
-  if(!id) return response.status(400).json({ message: "Invalid post data", post: null });
+  if(!id) return response.status(400).json({ message: "Invalid post data" , post: null });
   try {
     Post.findById(id).populate('comments').exec((error, post) => {
       if(error){
         return response.status(400).json({ message: e.message, post: null });
       }
       if(post){
-        return response.status(200).json({ message: "The post data", post });
+        return response.status(200).json({ message: "The post data" , post });
       }
     });
   } catch (e) {
@@ -162,10 +171,12 @@ module.exports.addViewToPostById = async (request, response) => {
     views: ++request.body.views
   }
   try {
-    const postUpdated = await Post.findOneAndUpdate({ _id: id }, update, { new: true });
+    const postUpdated = await Post.findOneAndUpdate({ _id: id }, update, {
+      new: true
+    });
     if(postUpdated){
-      // 204 - Success, without content creation 
-      response.status(204).json();
+      // 204 - Success, without content creation
+      return response.status(204).json();
     }
   } catch (e) {
     response.status(500).json(e);
