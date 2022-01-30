7bf0ace97c2d4d4d75956e013934add50a9c771b
AlyonaYAV <alyonayavorskaya03@gmail.com>
2022-01-30 21:06:50 +0200

    Changed 'default' layout. Added a 'sandwich' button that shows and hides the sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,19 @@
     <el-header height="70px" class="el-header-main-layout">
       <Navigation />
     </el-header>
-    <el-container>
+    <el-container class="el-container-main-layout">
+      <input type="checkbox" id="check" />
+      <label for="check">
+        <i>
+          <span></span>
+          <span></span>
+        </i>
+        <i>
+          <span></span>
+          <span></span>
+          <span></span>
+        </i>
+      </label>
       <side-bar  v-if="sidebar" :sidebarBlocks="sidebarBlocks" />
       <el-main>
         <Nuxt />
